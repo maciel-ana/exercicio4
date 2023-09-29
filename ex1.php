c01ef9a4046f8a7856f42fca05dc5c1461f2cd33
maciel-ana <130106962+maciel-ana@users.noreply.github.com>
2023-09-29 14:12:18 -0300

    commit maciel

--- ex1.php
+++ ex1.php
@@ -1,9 +1,9 @@
 <?php
-    $velocidade = 80;
+    $velocidade = 40;
 
-    if ($velocidade = 41 && $velocidade <= 50){
+    if ($velocidade >= 41 && $velocidade <= 50){
          echo "Você está na velocidade correta";
-    } elseif($velocidade == 40){
+    } elseif ($velocidade == 40){
         echo "Atenção! você esta a $velocidade km/h.";
     } else {
         echo "Você foi multado! Ultrapassou a velocidade máxima (50km/h)";
